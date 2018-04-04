e8843ae6ea21b2c6e63786a5f55472c8e6810519
yongchanlong <33020278+yongchanlong@users.noreply.github.com>
2018-04-04 16:39:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -ex
 if [ ! -d "/run/secrets/" ]; then
   echo '-=-~ not secrets file, so using default'
 else
-  export MOODLE_DB_PASS=$(< /run/secrets/db-password)
+  export MOODLE_DB_PASS=$(cat /run/secrets/db-password )
 fi
