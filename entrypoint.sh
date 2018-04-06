f4e8a4b8ed8ed10fb8ef0faff15621f3ea366128
yongchanlong <33020278+yongchanlong@users.noreply.github.com>
2018-04-06 13:01:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 if [ -d "/run/secrets/" ]; then
   export MOODLE_DB_PASS=$(cat /run/secrets/db-password )
 fi
+export ABC='sdfsdf'
 : "${MOODLE_DB_TYPE:?Lack of env variables}"
 : "${MOODLE_DB_HOST:?Lack of env variables}"
 : "${MOODLE_DB_NAME:?Lack of env variables}"
