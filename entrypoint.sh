92a04cd1905d5d43e423c839374cd619885acdf2
yongchanlong <33020278+yongchanlong@users.noreply.github.com>
2018-04-06 16:26:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,14 @@
+
 #!/bin/sh
+set -ex
+
 export ABC='sdfsdf'
+: "${MOODLE_DB_TYPE:?Lack of env variables}"
+: "${MOODLE_DB_HOST:?Lack of env variables}"
+: "${MOODLE_DB_NAME:?Lack of env variables}"
+: "${MOODLE_DB_USER:?Lack of env variables}"
+: "${MOODLE_DB_PASS?Lack of env variables}"
+: "${MOODLE_URL:?Lack of env variables}"
+
+chown apache:apache -R /var/moodledata
+exec /usr/sbin/httpd -D FOREGROUND
