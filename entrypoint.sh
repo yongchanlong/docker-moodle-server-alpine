5bd52e4be3c6eadaa699c68b20735c6c93f920de
yongchanlong <33020278+yongchanlong@users.noreply.github.com>
2018-04-06 15:42:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-if [ -d "/run/secrets/" ]; then
-  export MOODLE_DB_PASS=$(cat /run/secrets/db-password )
-fi
 export ABC='sdfsdf'
-
-chown apache:apache -R /var/moodledata
-exec /usr/sbin/httpd -D FOREGROUND
