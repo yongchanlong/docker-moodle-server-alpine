174cba41d474a842595c1d3b8a09fa9b5aadb717
yongchanlong <33020278+yongchanlong@users.noreply.github.com>
2018-04-10 09:56:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,6 @@
 : "${MOODLE_DB_USER:?Lack of env variables}"
 : "${MOODLE_DB_PASS?Lack of env variables}"
 : "${MOODLE_URL:?Lack of env variables}"
-if [ -d "/run/secrets/" ]; then
-  export MOODLE_DB_PASS=$(cat /run/secrets/db-password )
-fi
+
 chown apache:apache -R /var/moodledata
 exec /usr/sbin/httpd -D FOREGROUND
